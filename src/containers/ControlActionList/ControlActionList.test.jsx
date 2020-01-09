335889bc21359ac696552c6f6bc17ed1c93a7ce8
demmyhonore <demmyhonore@gmail.com>
2020-01-09 09:54:19 +0100

    feat(project): wrap graphql around test utils

--- src/containers/ControlActionList/ControlActionList.test.jsx
+++ src/containers/ControlActionList/ControlActionList.test.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { provided } from '../../testUtils';
+import { render } from '../../testUtils';
 import ControlActionList from './ControlActionList';
 
-test('renders without crashing', () => {
-  const div = document.createElement('div');
+describe('<ControlActionList />', () => {
+  test('matches snapshot', () => {
+    const { container } = render(<ControlActionList />);
 
-  ReactDOM.render(provided(<ControlActionList />), div);
+    expect(container).toMatchSnapshot();
+  });
 });
--- src/testUtils.jsx
+++ src/testUtils.jsx
@@ -3,22 +3,26 @@ import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from '@material-ui/styles';
 import { I18nextProvider } from 'react-i18next';
+import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'url-search-params-polyfill';
 import i18n from '../src/i18n';
+import client from './graphql';
 import theme from './theme';
 import store from './redux';
 
 const wrapper = ({ children }) => (
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <I18nextProvider i18n={i18n}>
-        <Router>
-          {children}
-        </Router>
-      </I18nextProvider>
-    </ThemeProvider>
-  </Provider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <I18nextProvider i18n={i18n}>
+          <Router>
+            {children}
+          </Router>
+        </I18nextProvider>
+      </ThemeProvider>
+    </Provider>
+  </ApolloProvider>
 );
 
 const customRender = (ui, options) => render(ui, { wrapper, ...options });
