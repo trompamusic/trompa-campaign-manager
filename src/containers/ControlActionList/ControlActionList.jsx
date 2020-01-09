b82f2673aad4b9fe6f60d537dc5992c2d76e96f3
demmyhonore <demmyhonore@gmail.com>
2020-01-09 12:05:59 +0100

    chore(project): refactor control actions query

--- src/containers/ControlActionList/ControlActionList.jsx
+++ src/containers/ControlActionList/ControlActionList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
 const CONTROL_ACTIONS_QUERY = gql`
-  query ControlActionList {
+  query ControlActions {
     ControlAction {
       identifier
       description
