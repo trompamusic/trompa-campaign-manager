e98cf146a86c300b5b9df6f08a2133d69398465b
demmyhonore <demmyhonore@gmail.com>
2020-01-17 11:44:30 +0100

    feat(campaign): add unit tests for active task

--- src/components/ActiveTask/ActiveTask.test.jsx
+++ src/components/ActiveTask/ActiveTask.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { getbyText } from '@testing-library/react';
 import { render } from '../../testUtils';
 import ActiveTask from './ActiveTask';
 
@@ -8,5 +8,23 @@ describe('<ActiveTask />', () => {
     const { container } = render(<ActiveTask />);
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('displays next task with task props', () => {
+    const { getByText } = render(<ActiveTask name="abc" url="https://demo.videodock.com/trompa/ce-task" />);
+
+    expect(getByText('Next task')).toBeTruthy();
+  });
+
+  test('displays loading with loading prop', () => {
+    const { getByText } = render(<ActiveTask loading={true} />);
+
+    expect(getByText('Loading...')).toBeTruthy();
+  });
+
+  test('displays no tasks left with task prop', () => {
+    const { getByText } = render(<ActiveTask noTasks={true} />);
+
+    expect(getByText('No tasks left')).toBeTruthy();
   });
 });
