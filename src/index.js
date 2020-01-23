b67ef7699c72e25cfc1429d972312620f90636fb
demmyhonore <demmyhonore@gmail.com>
2020-01-23 12:19:16 +0100

    chore(project): add ie11 to dev browserlist

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import polyfills
-import 'core-js/features/array/find';
 import 'core-js/features/object/values';
+import 'core-js/features/array/find';
 import 'core-js/features/array/every';
 import 'core-js/features/array/includes';
 import 'core-js/features/set';
