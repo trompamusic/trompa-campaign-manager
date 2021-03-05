0a4fa022af7e884d4655e928ec7ea2b7c0d43730
Roy Schut <roy@videodock.com>
2021-03-05 15:24:45 +0100

    chore(campaign): follow code convention

--- src/components/ActiveTask/ActiveTask.jsx
+++ src/components/ActiveTask/ActiveTask.jsx
@@ -34,7 +34,7 @@ export default function ActiveTask ({
 
   useEffect(() => {
     const iframeListener = ({ data }) => {
-      if(data === 'submit'){
+      if (data === 'submit'){
         handleSubmit();
       }
     };
